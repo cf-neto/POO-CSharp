85c83f0ad2fc802f6dd7f8b782ef9b2db064e5f9
cf-neto <carlosfdsn2008@gmail.com>
2025-04-09 17:23:54 -0300

    feat-adicionando-pasta

---
+++ Classes e Objetos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace POO
+{
+    public class Program
+    {
+        class Personagem
+        {
+            public string Nome;
+            public int Nivel;
+            public int Forca;
+
+            public Personagem(string nome, int nivel, int forca)
+            {
+                Nome = nome;
+                Nivel = nivel;
+                Forca = forca;
+                Console.WriteLine($"Nome do Personagem: {Nome} | Nivel: {Nivel} | Força {Forca}");
+            }
+
+            public void Atacar()
+            {
+                Console.WriteLine($"O personagem {Nome} atacou e deu {Nivel * Forca} de dano");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Personagem person = new Personagem("Thomas", 10, 2);
+            person.Atacar();
+        }
+    }
+}
