57a76c8ec3f89b24078143bdfff0ea24ef13ad9a
cf-neto <carlosfdsn2008@gmail.com>
2025-04-18 08:23:37 -0300

    feat-adicionando-estrutura-de-dados

---
+++ Estrutura-dados/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Estudando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> pilha = new Stack<string>();
+
+            pilha.Push("Livro A");
+            pilha.Push("Livro B");
+            pilha.Push("Livro C");
+
+            Console.WriteLine(pilha.Pop());
+            Console.WriteLine(pilha.Peek());
+
+            Console.WriteLine(" ");
+            foreach (string pil in pilha)
+            {
+                Console.WriteLine(pil);
+            }
+            
+
+            Queue<string> filaClientes = new Queue<string> ();
+
+            filaClientes.Enqueue("Carlos");
+            filaClientes.Enqueue("Fernando");
+            filaClientes.Enqueue("Bosta");
+
+            while (filaClientes.Count > 0)
+            {
+                Console.WriteLine($"Atendendo {filaClientes.Dequeue()}");
+            }
+
+            // HASHSET SERVE PARA MUITA COISA, ORDENAR, VERIFICAR SE TEM NA LISTA, OPERAÇÃO MATEMÁTICA E ETC.
+            List<int> ints = new List<int> {3, 2, 5, 4, 1};
+            HashSet<int> Ordernar = new HashSet<int>(ints);
+
+
+
+        }
+    }
+}
