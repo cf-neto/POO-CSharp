1a0bd29a6fabe7a4ba4e5388c96a416e6a6a40f9
cf-neto <carlosfdsn2008@gmail.com>
2025-04-21 10:07:26 -0300

    feat-colocando-linq

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -6,54 +6,32 @@ namespace Estudando
 {
     class Program
     {
-        class Filme
+
+        class clienteIdade
         {
             public string Nome { get; set; }
-            public string Genero { get; set; }
-            public int Duracao { get; set; }
+            public int Idade { get; set; }
         }
 
         static void Main(string[] args)
         {
-            var filmes = new List<Filme>
+            List<clienteIdade> clienteIdades = new List<clienteIdade>
             {
-                new Filme { Nome = "Velozes e Furiosos", Genero = "Ação", Duracao = 130 },
-                new Filme { Nome = "Invocação do Mal", Genero = "Terror", Duracao = 110 },
-                new Filme { Nome = "Titanic", Genero = "Romance", Duracao = 195 },
-                new Filme { Nome = "Pantera Negra", Genero = "Ação", Duracao = 120 },
-                new Filme { Nome = "Annabelle", Genero = "Terror", Duracao = 75 },
+                new clienteIdade {Nome = "Carlos", Idade = 18},
+                new clienteIdade {Nome = "Fernando", Idade = 25},
+                new clienteIdade {Nome = "Isadora", Idade = 12} 
             };
 
-            /// FILTRAR FILMES DE TERROR
+            Console.WriteLine("\n");
 
-            Console.WriteLine("Filmes de terror: ");
+            var verificar = from cliente in clienteIdades
+                            where cliente.Idade >= 18
+                            select cliente;
 
-            var terror = filmes
-                .Where(f => f.Genero == "Terror")
-                .Select(f => f.Nome);
-
-            foreach (var nome in terror)
+            foreach (var cliente in verificar)
             {
-                Console.WriteLine(nome);
+                Console.WriteLine($"Nome: {cliente.Nome} | Idade: {cliente.Idade}");
             }
-
-            /// FILTRAR FILMES COM A DURAÇÃO MENOR QUE 100
-            Console.WriteLine("\nFilmes com a duração menor que 100: ");
-
-            var filmes100 = filmes
-                .Where(f => f.Duracao < 100)
-                .Select(f => f.Nome);
-
-            foreach(var nome in filmes100)
-            {
-                Console.WriteLine(nome);
-            }
-
-            /// MÉDIA DE DURAÇÃO DOS FILMES
-
-            var duracao = filmes.Average(f => f.Duracao);
-            Console.WriteLine("\nDuração média dos filmes: ");
-            Console.WriteLine(duracao);
         }
     }
 }
